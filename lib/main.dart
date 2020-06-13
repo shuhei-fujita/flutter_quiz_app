ed17f3732a1085ea7d6bee33fe2bc9571ef5c444
syuheifujita <syuheifujita0117@gmail.com>
2020-06-13 23:27:50 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,63 @@
 import 'package:flutter/material.dart';
 
+import './answer.dart';
+import './question.dart';
+
 //void main() {
 //  runApp(MyApp());
 //}
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+
     var questions = [
-      'What is your favorite color?',
-      'What\'s your favorite animal?',
+      {
+        'questionText': 'What is your favorite color?',
+        'answers': ['Red', 'Blue', 'Yellow'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Dog', 'Cat', 'Tiger'],
+      },
+      {
+        'questionText': 'What\'s your favorite food?',
+        'answers': ['Ramen', 'Sushi', 'Soba'],
+      },
     ];
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Quiz App'),),
-      body: Column(
-        children: [
-          Text('The Question'),
-          RaisedButton(
-            child: Text('Answer 1'),
-          ),
-          RaisedButton(
-            child: Text('Answer 2'),
-          ),
-          RaisedButton(
-            child: Text('Answer 3'),
-          ),
-          RaisedButton(
-            child: Text('Answer 4'),
-          ),
-        ],
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Quiz App'),),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            })
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
---
+++ lib/question.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+
+  // finalは一度値が代入されたら，変更不可
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
